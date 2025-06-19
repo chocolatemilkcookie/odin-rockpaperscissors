46e59c9707dfd214340ccef728f2b83d1cd66da7
chocolatemilkcookie <187956214+chocolatemilkcookie@users.noreply.github.com>
2025-06-19 08:46:57 +0300

    allow game to be played by pressing buttons

--- rps.js
+++ rps.js
@@ -7,9 +7,7 @@ function getComputerChoice(){
     let computerChoice;
     random1 = Math.random();
     random2 = Math.floor(random1 * 3) + 1;
-    //console.log(random1);
-    //console.log(random2);
-
+    
     switch (random2){
         case 1:
             console.log("Computer choice: Rock");
@@ -35,110 +33,111 @@ function getComputerChoice(){
 
 }
 
-function getHumanChoice(){
-    let humanChoice;
-    let hchoice = prompt("Enter Rock, Paper or Scissors", "Rock");
 
-    if(hchoice.toLowerCase() === "rock"){
-        console.log("Human choice: Rock");
-        humanChoice = "rock";
-    }
-    else if(hchoice.toLowerCase() === "paper"){
-        console.log("Human choice: Paper");
-        humanChoice = "paper";
-
-    }
-    else if(hchoice.toLowerCase() === "scissors"){
-        console.log("Human choice: Scissors");
-        humanChoice = "scissors";
-
-    }
-    else{
-        console.log("Human Choice: Invalid");
-    }
-
-    return humanChoice;
-}
+let message;
 
 function playRound(humanChoice, computerChoice){
 
+
     if(humanChoice === "rock" && computerChoice === "rock"){
-        console.log("It is a tie, you both chose Rock");
+        message = ("It is a tie, you both chose Rock");
+        console.log(message);
         console.log("\n");
         return false;
     }
     else if(humanChoice === "rock" && computerChoice === "paper"){
-        console.log("You lose, Paper beats Rock");
+        message = ("You lose, Paper beats Rock");
+        console.log(message);
         console.log("\n");
         return false;
     }
     else if(humanChoice === "rock" && computerChoice === "scissors"){
-        console.log("You Win, Rock beats Scissors");
+        message = ("You Win, Rock beats Scissors");
+        console.log(message);
         console.log("\n");
         return true;
     }
     else if(humanChoice === "paper" && computerChoice === "rock"){
-        console.log("You win, Paper beats Rock");
+        message = ("You win, Paper beats Rock");
+        console.log(message);
         console.log("\n");
         return true;
     }
     else if(humanChoice === "paper" && computerChoice === "paper"){
-        console.log("It is a tie, you both chose Paper");
+        message = ("It is a tie, you both chose Paper");
+        console.log(message);
         console.log("\n");
         return false;
     }
     else if(humanChoice === "paper" && computerChoice === "scissors"){
-        console.log("You lose, Scissors beats");
+        message = ("You lose, Scissors beats paper");
+        console.log(message);
         console.log("\n");
         return true;
     }
     else if(humanChoice === "scissors" && computerChoice === "rock"){
-        console.log("You lose, Rock beats Scissors");
+        message = ("You lose, Rock beats Scissors");
+        console.log(message);
         console.log("\n");
         return false;
     }
     else if(humanChoice === "scissors" && computerChoice === "paper"){
-        console.log("You win Scissors beats Paper");
+        message = ("You win Scissors beats Paper");
+        console.log(message);
         console.log("\n");
         return true;
     }
     else if(humanChoice === "scissors" && computerChoice === "scissors"){
-        console.log("It is a tie, you both chose Scissors");
+        message = ("It is a tie, you both chose Scissors");
+        console.log(message);
         console.log("\n");
         return false;
     }
     else{
-        console.log("Could not get one of the values");
+        message = ("Could not get one of the values");
+        console.log(message);
         console.log("\n");
         return false;
     }
+
 }
 
 function playGame(){
     let count = 0;
     let wins;
-    for(i=1; i<=5;i++){
+    while (true) {
 
-        let humanSelection = getHumanChoice();
+
+        let humanSelection = humanChoice;
         let computerSelection = getComputerChoice();
         //console.log(wins = playRound(humanSelection,computerSelection));
 
         wins = playRound(humanSelection,computerSelection);
-        if(wins){
-            count++
-        }
     }
-    console.log(`You have won ${count} times`);
-    console.log("\n");
-
-    if(count >= 3){
-        console.log("Congratulations, You beat the computer");
-    }
-    else{
-        console.log("Ahh Shacks, You lost to a damn machine");
-    }
-    console.log("********************");
 }
 
-playGame()
+// playGame();
+
+
+
+let humanChoice;
+
+const buttons = document.querySelectorAll("button");
+let container = document.querySelector(".container1");
+let results = document.querySelector("#results");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        humanChoice = (button.id);
+        console.log(humanChoice);
+        let computerSelection = getComputerChoice();
+        playRound(humanChoice, computerSelection);
+        results.textContent = message;
+        console.log(`message is ${message}`);
+        container.appendChild(results);
+    });
+});
+
+
+
 
